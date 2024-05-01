3a8c12f9db1821be3c1225ec2404d02067596017
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-05-01 01:55:46 -0300

    feat: adding news routes in server

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,30 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import cookie from "@fastify/cookie";
 import fastify from "fastify";
-import { z } from "zod";
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
 
 const app = fastify();
 
-const prisma = new PrismaClient();
-
-app.get("/hello", () => {
-  return { message: "Hello World!" };
+app.register(cookie, {
+  secret: "polls-app",
+  hook: "onRequest",
 });
 
-app.post("/polls", async (request, reply) => {
-  const createPollBody = z.object({
-    title: z.string(),
-  });
-
-  const { title } = createPollBody.parse(request.body);
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    },
-  });
-
-  return reply.code(201).send({ pollId: poll.id });
-});
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
